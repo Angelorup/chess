47de702360897c60da7e8a40bcdd67d191446ba8
Angelo Ruperti <arruperti@gmail.com>
2020-01-27 14:53:45 +0100

    js for drag

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,13 @@
+
+
+const onClick = () => {
+    const select = document.querySelectorAll(".white");
+    select.forEach(white => {
+        white.addEventListener('click', (e) => {
+            console.log('hi');
+        })
+    })
+}
+onClick();
+// console.log('hi');
 import "bootstrap";
